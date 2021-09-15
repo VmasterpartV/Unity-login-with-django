ba36185cdb0644e6d57407d2346848e801f8b67d
Ivan Mota <ivan_mota09@hotmail.com>
2021-09-15 11:09:01 -0500

    Decode jwt token

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
 
 public class Login : MonoBehaviour
 {
     [SerializeField] public InputField username = null;
     [SerializeField] public InputField pass = null;
 
-    private readonly string Baseurl = "http://127.0.0.1:8000/api/v1.0/";
+    private readonly string Baseurl = "http://127.0.0.1:8000/api/";
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public class LoginTokens
@@ -33,10 +34,8 @@ public class Login : MonoBehaviour
         StartCoroutine(login());
     }
 
-    private IEnumerator login ()
+    private IEnumerator login()
     {
-        Debug.Log(username.text);
-        Debug.Log("iniciar sesión");
         WWWForm form = new WWWForm();
         form.AddField("username", username.text);
         form.AddField("password", pass.text);
@@ -56,15 +55,33 @@ public class Login : MonoBehaviour
             tokens = JsonUtility.FromJson<LoginTokens>(res);
             Debug.Log(tokens.access);
 
+            var parts = tokens.access.Split('.');
+            JToken userID = null;
+            if (parts.Length > 2)
+            {
+                var decode = parts[1];
+                var padLength = 4 - decode.Length % 4;
+                if (padLength < 4)
+                {
+                    decode += new string('=', padLength);
+                }
+                var bytes = System.Convert.FromBase64String(decode);
+                var accessData = System.Text.ASCIIEncoding.ASCII.GetString(bytes);
+                JObject json = JObject.Parse(accessData);
+                userID = json["id"];
+                Debug.Log(userID.ToString());
+            }
+
             /* GET USER */
-            string getUserUrl = Baseurl + "players/" + username.text + "/";
+            string getUserUrl = Baseurl + "players/" + userID.ToString() + "/";
             UnityWebRequest www = UnityWebRequest.Get(getUserUrl);
             yield return www.SendWebRequest();
 
             if (www.isNetworkError || www.isHttpError)
             {
                 Debug.Log(www.error);
-            } else
+            }
+            else
             {
                 Debug.Log(www.downloadHandler.text);
             }
